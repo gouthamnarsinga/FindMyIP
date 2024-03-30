19d39cecb7d4b6ae0594c38e98dfc4155105c781
gouthamnarsinga <gouthamnarsinga@gmail.com>
2024-03-30 17:49:22 +0530

    Added MainApp

---
+++ MainApp/MainApp/MainAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  MainAppApp.swift
+//  MainApp
+//
+//  Created by Danush on 30/03/24.
+//
+
+import SwiftUI
+import FindMyIPLibrary
+
+@main
+struct MainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FindMyIPContentView()
+        }
+    }
+}
